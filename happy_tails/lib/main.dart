8cf184d7cda80907dcaff08e4833849743bfe1a0
ESHAN <eshaninteser@gmail.com>
2026-08-24 10:58:13 +0600

    Splash Screen Added
    Upgraded Splash Screen

--- happy_tails/lib/main.dart
+++ happy_tails/lib/main.dart
@@ -3,11 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:happy_tails/constants/app_colors.dart';
 import 'package:happy_tails/screens/login_screen.dart';
 import 'package:happy_tails/widgets/logo_section.dart';
+
 void main() {
   runApp(const HappyTails());
 }
+
 class HappyTails extends StatelessWidget {
   const HappyTails({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,11 +19,13 @@ class HappyTails extends StatelessWidget {
     );
   }
 }
+
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
+
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
@@ -32,14 +37,13 @@ class _SplashScreenState extends State<SplashScreen> {
       );
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.background,
-      body: const SafeArea(
-        child: Center(
-          child: LogoSection(),
-        ),
+      body: const Center(
+        child: LogoSection(), // only the logo, perfectly centered
       ),
     );
   }
